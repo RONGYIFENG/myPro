d56f0bf3a85b906bf5a3b812ebfda3d6ee66eedc
best <ryf841203739@163.com>
2018-12-04 09:01:21 +0800

    ds

--- src/main/java/com/rong/app/boot/auth/controller/AuthController.java
+++ src/main/java/com/rong/app/boot/auth/controller/AuthController.java
@@ -6,34 +6,15 @@
 package com.rong.app.boot.auth.controller;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.alibaba.fastjson.JSON;
-import com.langyi.comp.excel.service.ExportExcelService;
-import tech.liveeasy.platform.common.plugs.code.DictCodeService;
-import tech.liveeasy.platform.common.plugs.pagination.Page;
-import tech.liveeasy.platform.common.plugs.pagination.RestfulResult;
-import tech.liveeasy.platform.common.utils.CommonUtils;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import com.rong.app.boot.auth.entity.AuthEntity;
+import com.rong.app.boot.auth.service.IAuthService;
+import com.rong.app.boot.utils.RestfulResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-
-
-import com.rong.app.auth.service.IAuthService;
-import com.rong.app.auth.vo.AuthVo;
-import com.rong.app.auth.entity.AuthEntity;
 
 /**
 * Auth
@@ -50,9 +31,6 @@ public class AuthController {
 
 	@Resource
 	private IAuthService authService;
-
-    @Autowired
-    private DictCodeService dictCodeService;
 	
 	/**
      * 显示Auth列表页面
@@ -60,55 +38,9 @@ public class AuthController {
     @RequestMapping("/index")
     public String bankInfoList(HttpServletRequest request,
                                HttpServletResponse response){
-        dictCodeService.config(AuthEntity.class);
-        request.setAttribute("dictJsonStr", dictCodeService.getJSONStr());
         return "/ops/auth/auth/view";
     }
 
-    /**
-     * 获取Auth列表数据
-     */
-    @RequestMapping("/pageList")
-    @ResponseBody
-    public RestfulResult pageList(@RequestBody Page<AuthEntity> page) throws Exception{
-        RestfulResult<Page<AuthEntity>> result = new RestfulResult();
-        page = authService.getAuthList(page);
-        result.setSuccess(true);
-        result.setMsg("查询成功");
-        result.setData(page);
-        return result;
-    }
-
-    /**
-     * 导出Auth数据
-     */
-    @RequestMapping("/export")
-    public void export(HttpServletRequest request, HttpServletResponse response,
-                                @RequestBody(required=false) AuthEntity authEntity) throws Exception {
-        ServletOutputStream out;
-        List<AuthVo> queryResult;
-        ExportExcelService service;
-        Workbook workbook;
-        Sheet sheet;
-
-        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("Auth_"+System.currentTimeMillis()+".xls", "UTF-8"));
-        response.setContentType("application/msexcel;charset=UTF-8");
-
-        queryResult =  authService.getPage(authEntity);
-        workbook = new HSSFWorkbook();
-        sheet = workbook.createSheet("Auth");
-        service = new ExportExcelService(queryResult, sheet, "Auth");
-        //装配编码
-        //service.addDic();
-        service.doExport();
-        //生成下拉数据和数据检测
-        service.exportTemplate();
-
-        out = response.getOutputStream();
-        workbook.write(out);
-        out.close();
-        out.flush();
-    }
 
     /**
      * 插入Auth数据
